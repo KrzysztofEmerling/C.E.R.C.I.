c94bdee37155609c7ab5a1c8de5995c54b37a2ba
KrzysztofEmerling <krzysztof.emerling@gmail.com>
2025-04-22 21:54:37 +0200

    Dokończenie generacji ruchów pseudo legalnych dla biaych

--- src/MoveGenerator/MoveGenerator.h
+++ src/MoveGenerator/MoveGenerator.h
@@ -13,13 +13,21 @@ struct Move
 class MoveGenerator
 {
 public:
-    static void GetLegalMovesQueue(const BoardState &state, std::queue<Move> &moves);
+    static void GetLegalMovesBBs(const BoardState &state, u64f &LegalMovesBBs[6]);
 private:
-    static u64 GetPseudoLegaWhitePawnsMoves(const u64f powns, const u64 empty, const u64 black_with_enpassants);
-    static u64 GetPseudoLegaBlackPawnsMoves(const u64f pawns, const u64 empty, const u64 white_with_enpassants);
+    static u64 GetPseudoLegalWhitePawnsMoves(const u64f powns, const u64 empty, const u64 black_with_enpassants);
+    static u64 GetPseudoLegalWhitePawnsMoves(const u64f powns, const u64 empty, const u64 black_with_enpassants, u64 &pownsAttacks);
+    static u64 GetPseudoLegalBlackPawnsMoves(const u64f pawns, const u64 empty, const u64 white_with_enpassants);
+    static u64 GetPseudoLegalBlackPawnsMoves(const u64f pawns, const u64 empty, const u64 white_with_enpassants, u64 &pownsAttacks);
     
-    static u64 GetPseudoLegaKnightsMove(const u64f powns, const u64 allay);
+    static u64 GetPseudoLegalKnightsMoves(const u64f knights, const u64 allay);
+    static u64 GetKnightsAttacks(const u64f knights);
+    
+    static u64 GetPseudoLegalRooksMoves(const u64f rooks, const u64 allay, const u64 blockers);
+    static u64 GetPseudoLegalBishopsMoves(const u64f bishops, const u64 allay, const u64 blockers);
+    static u64 GetPseudoLegalQueensMoves(const u64f queens, const u64 allay, const u64 blockers);
 
+    static u64 GetPseudoLegalKingMoves(const u64f king, const u64 allay);
 
     static inline constexpr u64 NOT_RANKS_1_2 = 0xFFFFFFFFFFFF0000;
     static inline constexpr u64 NOT_RANK_1 =    0xFFFFFFFFFFFFFF00;
@@ -31,3 +39,52 @@ private:
     static inline constexpr u64 NOT_COLS_AB =   0xFCFCFCFCFCFCFCFC;
     static inline constexpr u64 NOT_COLS_GH =   0x3F3F3F3F3F3F3F3F;
 };
+
+
+constexpr u64 GetBetweenSquaresConstexpr(int sq1, int sq2)
+{
+    if (sq1 == sq2) return 0ULL;
+
+    int file1 = sq1 % 8;
+    int rank1 = sq1 / 8;
+    int file2 = sq2 % 8;
+    int rank2 = sq2 / 8;
+
+    int df = (file2 > file1) ? 1 : (file2 < file1) ? -1 : 0;
+    int dr = (rank2 > rank1) ? 1 : (rank2 < rank1) ? -1 : 0;
+
+    // Sprawdzenie, czy pola leżą na linii prostej lub diagonalnej
+    if (df != 0 && dr != 0 && (abs(file2 - file1) != abs(rank2 - rank1)))
+        return 0ULL;
+
+    u64 between = 0ULL;
+    int f = file1 + df;
+    int r = rank1 + dr;
+
+    while (f != file2 || r != rank2)
+    {
+        between |= (1ULL << (r * 8 + f));
+        f += df;
+        r += dr;
+    }
+
+    // Usuń pole atakującego (ostatnie pole)
+    between &= ~(1ULL << sq2);
+
+    return between;
+}
+constexpr auto GenerateBetweenSquaresTable()
+{
+    std::array<std::array<u64, 64>, 64> table{};
+
+    for (int sq1 = 0; sq1 < 64; ++sq1)
+    {
+        for (int sq2 = 0; sq2 < 64; ++sq2)
+        {
+            table[sq1][sq2] = GetBetweenSquaresConstexpr(sq1, sq2);
+        }
+    }
+
+    return table;
+}
+constexpr auto BetweenSquaresTable = GenerateBetweenSquaresTable();
