66074e42a39b20476a17f5278ad49298498794ed
KrzysztofEmerling <krzysztof.emerling@gmail.com>
2025-05-14 14:33:54 +0200

    Zamiana std::queue na MovesList

--- src/MoveGenerator/Perft.cpp
+++ src/MoveGenerator/Perft.cpp
@@ -8,19 +8,16 @@ u64 PerftCount(BoardState &state, int depth)
     if (depth == 0)
         return 1;
 
-    std::queue<Move> moves;
-    MoveGenerator::GetLegalMoves(state, moves);
-
+    MoveList movesList;
+    MoveGenerator::GetLegalMoves(state, movesList);
     u64 nodes = 0;
 
-    while (!moves.empty())
+    for (int i = 0; i < movesList.movesCount; i++)
     {
-        Move move = moves.front();
-        moves.pop();
+        Move move = movesList.moves[i];
 
         BoardState newState = state;
         newState.MakeMove(move);
-
         nodes += PerftCount(newState, depth - 1);
     }
 
@@ -32,36 +29,27 @@ u64 Perft(BoardState &state, int depth)
     if (depth == 0)
         return 1;
 
-    std::queue<Move> moves;
-    MoveGenerator::GetLegalMoves(state, moves);
+    MoveList movesList;
+    MoveGenerator::GetLegalMoves(state, movesList);
+    u64 totalNodes = 0ULL;
+    u64 nodes = 0ULL;
 
-    u64 totalNodes = 0;
-
-    // później usuń!
-    // std::cout << state.GetBB(Empassants) << std::endl;
-
-    // Dla każdego ruchu pierwszego poziomu zliczamy nodes i wypisujemy
-    while (!moves.empty())
+    for (int i = 0; i < movesList.movesCount; i++)
     {
-        Move move = moves.front();
-        moves.pop();
+        Move move = movesList.moves[i];
 
         BoardState newState = state;
         newState.MakeMove(move);
+        nodes += PerftCount(newState, depth - 1);
 
-        u64 nodes = PerftCount(newState, depth - 1);
-
-        // Zamiana indeksów na notację UCI
         char f1 = 'a' + (move.startingSquere % 8);
         char r1 = '1' + (move.startingSquere / 8);
         char f2 = 'a' + (move.destSquere % 8);
         char r2 = '1' + (move.destSquere / 8);
 
         std::cout << f1 << r1 << f2 << r2 << ": " << nodes << std::endl;
-
         totalNodes += nodes;
     }
-
     std::cout << "Nodes searched: " << totalNodes << std::endl;
     return totalNodes;
 }
