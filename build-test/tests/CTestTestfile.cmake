9ed1f5ce052833e1b8a8f0c941dd85125f434f04
Krzysztof Emerling <91846025+KrzysztofEmerling@users.noreply.github.com>
2025-05-27 23:57:13 +0200

    Delete build-test directory

--- build-test/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/krzysztof/Documents/C.E.R.C.I./tests
-# Build directory: /home/krzysztof/Documents/C.E.R.C.I./build-test/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
